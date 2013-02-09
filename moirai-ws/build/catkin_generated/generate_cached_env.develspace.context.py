ed53730aa0c202597caada34b32247d8566832b0
ysuzuki <yumemi.suzuki@gmail.com>
2013-02-09 17:59:36 +0900

    reflesh ws

--- moirai-ws/build/catkin_generated/generate_cached_env.develspace.context.py
+++
@@ -1,8 +0,0 @@
-catkin_EXTRAS_DIR="/opt/ros/groovy/share/catkin/cmake"
-
-ENV_SCRIPT="/home/ysuzuki/moirai-ws/devel/env.sh"
-OUTPUT_SCRIPT="/home/ysuzuki/moirai-ws/build/catkin_generated/env_cached.sh"
-
-PREPEND_SPACE_DIR="/home/ysuzuki/moirai-ws/devel"
-CMAKE_PREFIX_PATH="/home/ysuzuki/moirai-ws/devel;/opt/ros/groovy".split(';') if "/home/ysuzuki/moirai-ws/devel;/opt/ros/groovy" != "" else []
-PYTHON_INSTALL_DIR="lib/python2.7/dist-packages"
